d7d14ac48df5538c79b02b395baa0fb3caa1a2ea
Cassia <ra-22265008-2@alunos.unicesumar.edu.br>
2024-04-16 18:13:24 -0300

    Character Adapter

--- adapter/character.adapter.ts
+++ adapter/character.adapter.ts
@@ -2,21 +2,19 @@ import { ImageAdapter } from "./image.adapter";
 import { CharacterExternal } from "dto/external/character-external.dto";
 import { Adapter } from "./adapter";
 import { Character } from "types/character.types";
-import { ImageRepository } from "repository/image.repository";
-import { CharacterRepository } from "repository/character.repository";
 
 export class CharacterAdapter implements Adapter<CharacterExternal, Character> {
-  private characterRepository: CharacterRepository = new CharacterRepository();
   private imageAdapter: ImageAdapter = new ImageAdapter();
-  private ImageRepository: ImageRepository = new ImageRepository();
 
   public async toInternal(external: CharacterExternal): Promise<Character> {
+    const image = await this.imageAdapter.toInternal(external.thumbnail);
+
     return {
       name: external.name,
       description: external.description,
       modified: new Date(external.modified),
       resourceURI: external.resourceURI,
-      // thumbnail:
+      thumbnail: image,
       comics: [],
       stories: [],
       events: [],
